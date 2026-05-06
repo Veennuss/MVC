a88b7d9624f740c8d4c6607f5508eb1d16617f1d
Aluno <e023aluno@outlook.com>
2026-05-06 17:58:51 -0300

    Atualização

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,20 +6,13 @@ dotenv.config();
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST as string,
+    user: process.env.DB_USER as string,
+    password: process.env.DB_PASSWORD as string,
+    database: process.env.DB_DATABASE as string,
     port: Number(process.env.DB_PORT),
-
-
-
     waitForConnections: true,
-
     connectionLimit: 10,
-    
     queueLimit: 0
-
 })
-
 export {pool}
